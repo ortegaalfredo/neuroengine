49357feff0e42737f0531bb431ea51d97b3418ad
ortegaalfredo <ortegaalfredo@gmail.com>
2023-06-13 05:37:51 -0300

    Server now tries to reconnect automatically.

--- neuroengine.py
+++ neuroengine.py
@@ -1,17 +1,12 @@
 import socket
+import select
+import time
 import json
 import http.client
 import ssl
 
 # Client class
 class Neuroengine:
-    server_address:str
-    server_port:int
-    service_name:str
-    verify_ssl:bool
-    key:str
-
-
     #__init__(): Initializes a new instance of the neuroengine class.
     #Parameters:
     #   server_address: A string representing the server address.
@@ -19,12 +14,15 @@ class Neuroengine:
     #   service_name: A string representing the name of the service.
     #   key (optional): A string representing an optional key.
     #   verify_ssl (optional): A boolean indicating whether to verify SSL certificates or not.
+
     def __init__(self, service_name, server_address="api.neuroengine.ai",server_port=443,key="",verify_ssl=True):
         self.server_address=server_address
         self.server_port=server_port
         self.service_name=service_name
         self.key=key
         self.verify_ssl=verify_ssl
+
+    #getmodel(): Return a list of active LLM models in the server
 
     def getModels(self):
         command = {'command': 'getmodels' }
@@ -43,7 +41,6 @@ class Neuroengine:
     #Returns:
     #   A string containing the response from the server.
 
-
     def request(self, prompt,temperature=1.0,top_p=0.9,top_k=40,repetition_penalty=1.2,max_new_len=128,seed=0):
         # Create a JSON message
         command = {
@@ -55,7 +52,11 @@ class Neuroengine:
             'max_new_len':max_new_len,
             'seed':seed
         }
-        response=self.send(command)
+        try:
+            response=self.send(command)
+        except:
+            response={}
+            response["reply"]="Connection error"
         return response["reply"]
 
     def send(self,command):
@@ -79,11 +80,6 @@ class Neuroengine:
 # Server class, use it to share your LLM
 
 class NeuroengineServer:
-    server_address:str
-    server_port:int
-    service_name:str
-    key:str
-
 
     #__init__(): Initializes a new instance of the neuroengine Server class
     #Parameters:
@@ -94,15 +90,15 @@ class NeuroengineServer:
     def __init__(self, server_address="api.neuroengine.ai",server_port=4444):
         self.server_address=server_address
         self.server_port=server_port
+
+    def login(self,service_name,service_key):
         # Create a socket object
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # Wrap the socket with SSL
         ssl_context = ssl.create_default_context()
-        self.ssl_socket = ssl_context.wrap_socket(self.client_socket,server_hostname=server_address)
+        self.ssl_socket = ssl_context.wrap_socket(self.client_socket,server_hostname=self.server_address)
         # Connect to the server
-        self.ssl_socket.connect((server_address,server_port))
-
-    def login(self,service_name,service_key):
+        self.ssl_socket.connect((self.server_address,self.server_port))
         # Login to server
         logindata={}
         logindata["user"]=service_name
@@ -111,25 +107,61 @@ class NeuroengineServer:
         self.ssl_socket.sendall(data.encode())
         response = self.ssl_socket.recv(10240)
         if (response.strip().decode()=="OK"):
+            self.service_name=service_name
+            self.service_key=service_key
+            self.pingtime=time.time()
             return(0)
         else:
             self.ssl_socket.shutdown(socket.SHUT_WR)
             return(-1)
 
+    def is_socket_closed(self,sock):
+        if time.time()-self.pingtime>60:
+            return True
+        else: 
+            return False
+
+    def has_bytes_to_receive(self,sock):
+        try:
+            r, _, _ = select.select([sock], [], [], 0)
+            return sock in r
+        except socket.error:
+            return False
+
+
     def listen(self,answerCallback):
             # Listen for data from the server
         while True:
-            data = self.ssl_socket.recv(10240)
-            if not data:
-                continue
-            received_string = data.decode()
-            request = json.loads(received_string)
-            # We received a keep-alive ping, ignore
-            if ("ping" in request):
-                continue
-            # Process request
-            response=answerCallback(request).encode()
-            # Send len
-            self.ssl_socket.sendall(("%08X" % len(response)).encode())
-            # Send message
-            self.ssl_socket.sendall(response)
+            # Attempt reconnect if server is down
+            while(self.is_socket_closed(self.client_socket)):
+                print("Neuroengine.ai: connection error, retrying...")
+                time.sleep(5)
+                try:
+                    self.login(self.service_name,self.service_key)
+                except: pass
+            try:
+                # Check if there are bytes in the socket
+                if self.has_bytes_to_receive(self.ssl_socket)==False:
+                    time.sleep(1)
+                    continue
+                # Read bytes
+                data = self.ssl_socket.recv(10240)
+                if not data:
+                    self.pingtime=0
+                    continue
+                received_string = data.decode()
+                request = json.loads(received_string)
+                # We received a keep-alive ping
+                if ("ping" in request):
+                    self.pingtime=time.time()
+                    continue
+                # Process request
+                response=answerCallback(request).encode()
+                # Send len
+                self.ssl_socket.sendall(("%08X" % len(response)).encode())
+                # Send message
+                self.ssl_socket.sendall(response)
+            except Exception as e:
+                print(f"Error: {str(e)}")
+                self.pingtime=0
+                pass
